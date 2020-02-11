e55db0a4a7bab4ecd5074eb7be0c8c13185a5eb9
Dominik Zettl <dominikzettl@gmx.net>
2020-02-11 19:14:08 +0100

    ...

---
+++ CLizer.Tests/PropertyBindingTests.cs
@@ -0,0 +1,53 @@
+using Clizer.Contracts;
+using Clizer.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CLizer.Tests
+{
+    public class PropertyBindingTests : ICliCmd
+    {
+        [Range(1, 100)]
+        public int Number { get; set; }
+        public bool Force { get; set; }
+
+
+        [Fact]
+        public async Task TestWrongProperty()
+        {
+            var args = new List<string>()
+            {
+                "test"
+            };
+
+            var clizer = new Clizer.Clizer();
+            clizer.Configure().AddCommandContainer(new CommandContainer(typeof(PropertyBindingTests)));
+            var result = await clizer.Execute(args.ToArray(), default);
+            Assert.Equal((int)ClizerExitCodes.ERROR, result);
+        }
+
+        [Fact]
+        public async Task TestCorrectBindingProperty()
+        {
+            var args = new List<string>()
+            {
+                "number:1",
+                "force"
+            };
+
+            var clizer = new Clizer.Clizer();
+            clizer.Configure().AddCommandContainer(new CommandContainer(typeof(PropertyBindingTests)));
+            var result = await clizer.Execute(args.ToArray(), default);
+            Assert.Equal((int)ClizerExitCodes.SUCCESS, result);
+        }
+
+        [SuppressMessage("Usage", "xUnit1013:Public method should be marked as test", Justification = "<Pending>")]
+        public async Task<int> Execute(CancellationToken cancellationToken)
+            => await Task.FromResult(Force && Number > 0 ? (int)ClizerExitCodes.SUCCESS : (int)ClizerExitCodes.ERROR);
+    }
+}
+

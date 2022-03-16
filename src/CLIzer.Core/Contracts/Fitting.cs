311324911a4c2a9e427703cef340efb306d11741
Dominik Zettl <dominikzettl@gmx.net>
2022-03-16 07:22:44 +0100

    Refactor

---
+++ src/CLIzer.Core/Contracts/Fitting.cs
@@ -0,0 +1,23 @@
+﻿namespace CLIzer.Core.Contracts
+{
+    public enum Fitting
+    {
+        Border, Content, Custom
+    }
+
+    internal class Position
+    {
+        public int LX { get; }
+        public int LY { get; }
+        public int BX { get; }
+        public int BY { get; }
+
+        public Position(int lX, int lY, int bx, int by)
+        {
+            LX = lX;
+            LY = lY;
+            BX = bx;
+            BY = by;
+        }
+    }
+}
